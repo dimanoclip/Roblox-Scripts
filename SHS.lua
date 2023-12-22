bf00b9ae2481deeb775029abe8173611fb093462
Dimanoname <gamefolover2019@gmail.com>
2023-12-22 08:26:51 +0200

    1

--- SHS.lua
+++ SHS.lua
@@ -12,18 +12,33 @@ local add = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanonam
 --AntiAFK
 LP.Idled:Connect(function() vu:CaptureController() vu:ClickButton2(Vector2.new()) end)
 --Main
+task.spawn(function()
 while task.wait() do
-	remote:FireServer("Upgrade", "Fusion")
-	remote:FireServer("Upgrade", "Realm")
-	for i,v in pairs(game:GetService("Workspace").TrainingZones:children()) do
-		if v.Name ~= "Other" and v.Name ~= "Speed" then
-			for i1,v1 in pairs(v:children()) do
-				if add.dist(v1.TrainingZone.Position) <= 15 and add.isstill(hum) then
-					remote:FireServer("Train", v.Name)
+		for i,v in pairs(game:GetService("Workspace").TrainingZones:children()) do
+			if v.Name ~= "Other" and v.Name ~= "Speed" then
+				for i1,v1 in pairs(v:children()) do
+					if add.dist(v1.TrainingZone.Position) <= 15 and add.isstill(hum) then
+						remote:FireServer("Train", v.Name)
+					end
 				end
 			end
 		end
+		remote:FireServer("Train Speed")
 	end
-	remote:FireServer("Train Speed")
-end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+end)
+task.spawn(function()
+	while task.wait(5) do
+		remote:FireServer("Upgrade", "Fusion")
+		remote:FireServer("Upgrade", "Realm")
+	end
+end)
+
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
+
+-- for i,v in pairs(game:GetService("Workspace").ChestSpawns.Gem:children()) do
+-- 	if v:FindFirstChild("GemChest") then
+-- 		game.Players.LocalPlayer.Character.PrimaryPart.CFrame = v:FindFirstChild("GemChest").Frame.CFrame
+-- 		v:FindFirstChild("GemChest").ProximityPrompt:InputHoldBegin()
+-- 	end
+-- 	task.wait(1)
+-- end
