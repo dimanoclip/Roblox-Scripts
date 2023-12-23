dfa232b87f0883a7f9c003714ace7d8f59e1d924
Dimanoname <gamefolover2019@gmail.com>
2023-12-23 15:24:24 +0200

    1

--- SHS.lua
+++ SHS.lua
@@ -5,13 +5,17 @@ if game.PlaceId ~= PlaceId then warn(string.format("Wrong game detected. %s expe
 local pp,LP = game.Players,game.Players.LocalPlayer
 local char = LP.Character or LP.CharacterAdded:Wait()
 local hum = char:WaitForChild("Humanoid")
-local rep,remote = game:GetService("ReplicatedStorage"),game:GetService("ReplicatedStorage"):WaitForChild("Remote")
+local rs,remote = game:GetService("ReplicatedStorage"),game:GetService("ReplicatedStorage"):WaitForChild("Remote")
 local vu = game:GetService("VirtualUser")
 --Libs
-local add = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/additional.lua"))()
+local adds = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/additional.lua"))()
 --AntiAFK
 LP.Idled:Connect(function() vu:CaptureController() vu:ClickButton2(Vector2.new()) end)
 --Main
+task.spawn(function()
+	remote:FireServer("Daily Draw", "VIP")
+	remote:FireServer("Daily Draw", "Free")
+end)
 task.spawn(function()
 	while task.wait() do
 		remote:FireServer("Time Quest", "Finished", "D1")
@@ -23,7 +27,7 @@ task.spawn(function()
 		for i,v in pairs(game:GetService("Workspace").TrainingZones:children()) do
 			if v.Name ~= "Other" and v.Name ~= "Speed" and v.Name ~= "Endurance" then
 				for i1,v1 in pairs(v:children()) do
-					if add.dist(v1.TrainingZone.Position) <= 15 and not add.moving(hum) then
+					if adds.dist(v1.TrainingZone.Position) <= 15 and not adds.moving(hum) then
 						remote:FireServer("Train", v.Name)
 					end
 				end
