11d27ff9509acadb21b5f28c1ea6b93d584d0845
Dimanoname <gamefolover2019@gmail.com>
2024-01-13 12:51:38 +0200

    1

--- Kaizen.lua
+++ Kaizen.lua
@@ -1,10 +1,29 @@
 local add = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/additional.lua"))()
 local lp = game.Players.LocalPlayer
-local Players = game:GetService("Players")
 local rs = game:GetService("ReplicatedStorage")
-local cancelquest = rs:WaitForChild("Knit"):WaitForChild("Services"):WaitForChild("questService"):WaitForChild("RE"):WaitForChild("CancelCurrentQuest")
+local cancelquest = rs.Knit.Services.questService.RE.CancelCurrentQuest
+local talk = rs.Knit.Services.interactService.RF.GetOptionData
+local invite = rs.Knit.Services.partyService.RE.Invite
 game.Players.LocalPlayer.Idled:Connect(function() game:GetService("VirtualUser"):CaptureController(); game:GetService("VirtualUser"):ClickButton2(Vector2.new()) end)
-local function nquest()
+local questTable = {
+    {quest = "BanditSideQuest", target = "Bandits", level = 1},
+    {quest = "BanditSideQuest", target = "Armed Bandits", level = 15},
+    {quest = "ShroomSideQuest", target = "Shrooms", level = 30},
+    {quest = "StudentSideQuest", target = "Juniors", level = 45},
+    {quest = "StudentSideQuest", target = "Seniors", level = 65},
+    {quest = "EnrolmentCurseQuest", target = "Fly Heads", level = 80},
+    {quest = "EnrolmentCurseQuest", target = "Fire Shrooms", level = 110},
+    {quest = "MidoriPrimarySideQuests", target = "Rogue Sorcerers", level = 125},
+    {quest = "MidoriPrimarySideQuests", target = "Crabions", level = 140},
+    {quest = "MidoriPrimarySideQuests", target = "Gnashers", level = 160},
+    {quest = "SwampPrimarySideQuest", target = "Cursed Sushis", level = 175},
+    {quest = "SwampPrimarySideQuest", target = "Poison Shrooms", level = 195},
+    {quest = "SwampPrimarySideQuest", target = "Eye Fanatics", level = 220},
+    {quest = "Miwa", target = "Saku", 70},
+    {quest = "Yuzi2", target = "Hanamato", 130},
+    {quest = "Pygmy", target = "Joko", 205}
+}
+local function nearestquest()
     for i,v in pairs(game:GetService("Workspace").Interactions.Sidequests:children()) do
         for i1,Quest in pairs(v:children()) do
             if add.dist_to(Quest.Position) <= 15 then
@@ -16,69 +35,45 @@ local function nquest()
     return ""
 end
 local function tptoquest(questname)
-    for i,v in pairs(game:GetService("Workspace").Interactions.Sidequests:children()) do
-        for i1,Quest in pairs(v:children()) do
-            if Quest.Name == questname and add.dist_to(Quest.Position) > 20 then
-                lp.Character.PrimaryPart.CFrame = Quest.CFrame
+    for _,k in pairs(game:GetService("Workspace").Interactions:children()) do
+        if k.Name == "Sidequests" or k.Name == "Story" then
+            for i,v in pairs(k:children()) do
+                for i1,Quest in pairs(v:children()) do
+                    if Quest.Name == questname and add.dist_to(Quest.Position) > 20 then
+                        lp.Character.PrimaryPart.CFrame = Quest.CFrame
+                    end
+                end
             end
         end
     end
 end
 local function getquest()
     local level = tonumber(string.split(lp.PlayerGui.UI.Tabs.MenuButton.Level.Text, "LV. ")[2])
-    if level >= 205 then tptoquest("Pygmy")
-    elseif level >= 195 then tptoquest("SwampPrimarySideQuest")
-    elseif level >= 175 then tptoquest("SwampPrimarySideQuest")
-    elseif level >= 160 then tptoquest("MidoriPrimarySideQuests")
-    elseif level >= 140 then tptoquest("MidoriPrimarySideQuests")
-    elseif level >= 130 then tptoquest("Yuzi2")
-    elseif level >= 125 then tptoquest("MidoriPrimarySideQuests")
-    elseif level >= 110 then tptoquest("EnrolmentCurseQuest")
-    elseif level >= 80 then tptoquest("EnrolmentCurseQuest")
-    elseif level >= 70 then tptoquest("Miwa")
-    elseif level >= 65 then tptoquest("StudentSideQuest")
-    elseif level >= 45 then tptoquest("StudentSideQuest")
-    elseif level >= 30 then tptoquest("ShroomSideQuest")
-    elseif level >= 15 then tptoquest("BanditSideQuest")
-    elseif level >= 1 then tptoquest("BanditSideQuest") end
+    for i,v in pairs(questTable) do
+        if v.level > level then
+            tptoquest(questTable[i-1].quest)
+        end
+    end
 end
 local function gettarget()
     local level = tonumber(string.split(lp.PlayerGui.UI.Tabs.MenuButton.Level.Text, "LV. ")[2])
-    if level >= 205 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Joko")
-    elseif level >= 195 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Poison Shrooms")
-    elseif level >= 175 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Cursed Sushis")
-    elseif level >= 160 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Gnashers")
-    elseif level >= 140 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Crabions")
-    elseif level >= 130 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Hanamato")
-    elseif level >= 125 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Rogue Sorcerers")
-    elseif level >= 110 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Fire Shrooms")
-    elseif level >= 80 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Fly Heads")
-    elseif level >= 70 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Saku")
-    elseif level >= 65 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Seniors")
-    elseif level >= 45 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Juniors")
-    elseif level >= 30 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Shrooms")
-    elseif level >= 15 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Armed Bandits")
-    elseif level >= 1 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Bandits") end
-    task.wait(0.1)
-    rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Confirm")
+    for i,v in pairs(questTable) do
+        if v.level > level then
+            talk:InvokeServer(nearestquest(), questTable[i-1].target)
+        end
+    end
+    task.wait(0.15)
+    talk:InvokeServer(nearestquest(), "Confirm")
 end
 local owner = "dimasikprofi4"
 game.Players:WaitForChild(owner).Chatted:Connect(function(text)
-    if text == "here" then
-        lp.Character.PrimaryPart.CFrame = game.Players["dimasikprofi4"].Character.PrimaryPart.CFrame
-    elseif text == "take" then
-        getquest()
-        task.wait(0.5)
-        gettarget()
-    elseif text == "done" then
-        rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Bye")
-    elseif text == "cancel" then
+    if text == "cancel" then
         cancelquest:FireServer()
     elseif text == "party" then
-        rs.Knit.Services.partyService.RE.Invite:FireServer(owner)
-    elseif text == "re" then
-        rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nquest(), "Bye")
-        task.wait(0.1)
+        invite:FireServer(owner)
+    elseif text == "r" then
+        talk:InvokeServer(nearestquest(), "Bye")
+        task.wait(0.25)
         getquest()
         task.wait(0.5)
         gettarget()
@@ -89,3 +84,58 @@ lp.Chatted:Connect(function(text)
         owner = text
     end
 end)
+
+
+
+-- local lp = game.Players.LocalPlayer
+-- local enabled, enemy, quest = true, nil, nil
+-- local levelLabel = game.Players.LocalPlayer.PlayerGui:FindFirstChild("UI"):FindFirstChild("Tabs"):FindFirstChild("MenuButton"):FindFirstChild("Level")
+-- local plylevel = tonumber(string.split(levelLabel.Text, "LV. ")[2])
+-- local questFrameLabel = lp.PlayerGui:WaitForChild("UINoReset"):FindFirstChild("Quests"):FindFirstChild("QuestsFrame").QuestTemplate1.Label
+
+-- game.Players.LocalPlayer.Idled:Connect(function() game:GetService("VirtualUser"):CaptureController(); game:GetService("VirtualUser"):ClickButton2(Vector2.new()) end)
+
+-- local questTable = {
+--     {"BanditSideQuest", "Bandits", 1},
+--     {"BanditSideQuest", "Armed Bandits", 15},
+--     {"ShroomSideQuest", "Shrooms", 30},
+--     {"StudentSideQuest", "Juniors", 45},
+--     {"StudentSideQuest", "Seniors", 65},
+--     {"EnrolmentCurseQuest", "Fly Heads", 80},
+--     {"EnrolmentCurseQuest", "Fire Shrooms", 110},
+--     {"MidoriPrimarySideQuests", "Rogue Sorcerers", 125},
+--     {"MidoriPrimarySideQuests", "Crabions", 140},
+--     {"MidoriPrimarySideQuests", "Gnashers", 160},
+--     {"SwampPrimarySideQuest", "Cursed Sushis", 175},
+--     {"SwampPrimarySideQuest", "Poison Shrooms", 195},
+--     {"SwampPrimarySideQuest", "Eye Fanatics", 220},
+--     {"Miwa", "Saku", 70},
+--     {"Yuzi2", "Hanamato", 130},
+--     {"Pygmy", "Joko", 205}
+-- }
+
+-- task.spawn(function()
+--     while task.wait() do
+--         for i=1,#questTable do
+--             if plylevel < tonumber(questTable[i][3]) then enemy = tostring(questTable[i-1][2]); quest = tostring(questTable[i-1][1]) end
+--         end
+--         if questFrameLabel.RichText then questFrameLabel.RichText = false end
+--         if quest ~= nil and enemy ~= nil then
+--             if string.find(questFrameLabel.Text, "rgb%(255, 255, 0%)") ~= nil then
+--                 game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetOptionData:InvokeServer(quest, "Bye")
+--             else
+--                 game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetOptionData:InvokeServer(quest, enemy)
+--                 game:GetService("ReplicatedStorage").Knit.Services.interactService.RF.GetOptionData:InvokeServer(quest, "Confirm")
+--             end
+--         end
+--     end
+-- end)
+-- game.Players["dimasikprofi4"].Chatted:Connect(function(text)
+--     if text == "/here" then
+--         lp.Character.PrimaryPart.CFrame = game.Players["dimasikprofi4"].Character.PrimaryPart.CFrame
+--     elseif text == "/toggle" then
+--         if enabled then enabled = false else enabled = true end
+--     elseif text == "/cancel" then
+--         game:GetService("ReplicatedStorage"):WaitForChild("Knit"):WaitForChild("Services").questService.RE.CancelCurrentQuest:FireServer()
+--     end
+-- end)
