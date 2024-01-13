4f0bbbf2711448f3a8e22e1779ed9a7efe3403a9
Dimanoname <gamefolover2019@gmail.com>
2024-01-13 13:07:57 +0200

    1

--- Kaizen.lua
+++ Kaizen.lua
@@ -17,28 +17,24 @@ local function nearestquest()
     return ""
 end
 local function tptoquest(questname)
-    for _,k in pairs(game:GetService("Workspace").Interactions.Sidequests:children()) do
-        for i,v in pairs(k:children()) do
-            for i1,Quest in pairs(v:children()) do
-                if Quest.Name == questname and add.dist_to(Quest.Position) > 20 then
-                    lp.Character.PrimaryPart.CFrame = Quest.CFrame
-                end
+    for _,v in pairs(game:GetService("Workspace").Interactions.Sidequests:children()) do
+        for i1,Quest in pairs(v:children()) do
+            if Quest.Name == questname and add.dist_to(Quest.Position) > 20 then
+                lp.Character.PrimaryPart.CFrame = Quest.CFrame
             end
         end
     end
 end
 local function getquest()
     local level = tonumber(string.split(lp.PlayerGui.UI.Tabs.MenuButton.Level.Text, "LV. ")[2])
-    if level >= 205 then tptoquest("Pygmy")
+    if level >= 220 then tptoquest("SwampPrimarySideQuest")
     elseif level >= 195 then tptoquest("SwampPrimarySideQuest")
     elseif level >= 175 then tptoquest("SwampPrimarySideQuest")
     elseif level >= 160 then tptoquest("MidoriPrimarySideQuests")
     elseif level >= 140 then tptoquest("MidoriPrimarySideQuests")
-    elseif level >= 130 then tptoquest("Yuzi2")
     elseif level >= 125 then tptoquest("MidoriPrimarySideQuests")
     elseif level >= 110 then tptoquest("EnrolmentCurseQuest")
     elseif level >= 80 then tptoquest("EnrolmentCurseQuest")
-    elseif level >= 70 then tptoquest("Miwa")
     elseif level >= 65 then tptoquest("StudentSideQuest")
     elseif level >= 45 then tptoquest("StudentSideQuest")
     elseif level >= 30 then tptoquest("ShroomSideQuest")
@@ -47,16 +43,14 @@ local function getquest()
 end
 local function gettarget()
     local level = tonumber(string.split(lp.PlayerGui.UI.Tabs.MenuButton.Level.Text, "LV. ")[2])
-    if level >= 205 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Joko")
+    if level >= 220 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Joko")
     elseif level >= 195 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Poison Shrooms")
     elseif level >= 175 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Cursed Sushis")
     elseif level >= 160 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Gnashers")
     elseif level >= 140 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Crabions")
-    elseif level >= 130 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Hanamato")
     elseif level >= 125 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Rogue Sorcerers")
     elseif level >= 110 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Fire Shrooms")
     elseif level >= 80 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Fly Heads")
-    elseif level >= 70 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Saku")
     elseif level >= 65 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Seniors")
     elseif level >= 45 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Juniors")
     elseif level >= 30 then rs.Knit.Services.interactService.RF.GetOptionData:InvokeServer(nearestquest(), "Shrooms")
