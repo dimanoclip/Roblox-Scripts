bc97b49861e30c7bbfeb1690b03fc82d96195b4f
Dimanoname <gamefolover2019@gmail.com>
2024-04-19 20:18:47 +0300

    1

--- dungquest.lua
+++ dungquest.lua
@@ -5,7 +5,6 @@ local plys = game.Players
 local lp = plys.LocalPlayer
 local char = lp.Character or lp.CharacterAdded:Wait()
 lp.CharacterAdded:Connect(function(character) char = character end)
-local root = char:WaitForChild("HumanoidRootPart")
 local rs = game:GetService("ReplicatedStorage")
 local chatevent = rs:WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
 local spawn = task.spawn
@@ -13,20 +12,10 @@ local pgui = lp.PlayerGui
 local abils = pgui.abilities.Frame
 local skill1 = abils.LeftAbility
 local skill2 = abils.RightAbility
-local lastpos = root.CFrame
-local function getcd(skill)
-    return (skill:FindFirstChild("slot") and skill:FindFirstChild("slot"):FindFirstChild("cooldownNumber")) and skill.slot.cooldownNumber.Text
-end
-local function delaymsg(target)
-    local args = {
-        [1] = "COOLDOWN!",
-        [2] = string.format("To %s", target.Name or target)
-    }
-    for i=1,5 do task.wait(); chatevent:FireServer(unpack(args)) end
-end
+local lastpos = char:FindFirstChild("HumanoidRootPart").CFrame
 local cmds = {
-    ["heal 1"] = function(target) if add.is_alive(target) and (getcd(skill1) == "0.1" or getcd(skill1) == "4") then root.CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.Q); task.wait(4); root.CFrame = lastpos elseif getcd(skill1) ~= "0.1" and getcd(skill1) ~= "4" then delaymsg(target) end end,
-    ["heal 2"] = function(target) if add.is_alive(target) and (getcd(skill2) == "0.1" or getcd(skill2) == "4") then root.CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.E); task.wait(4); root.CFrame = lastpos elseif getcd(skill2) ~= "0.1" and getcd(skill2) ~= "4" then delaymsg(target) end end
+    ["heal 1"] = function(target) if add.is_alive(target) then char:FindFirstChild("HumanoidRootPart").CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.Q); task.wait(15); char:FindFirstChild("HumanoidRootPart").CFrame = lastpos end end,
+    ["heal 2"] = function(target) if add.is_alive(target) then char:FindFirstChild("HumanoidRootPart").CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.E); task.wait(15); char:FindFirstChild("HumanoidRootPart").CFrame = lastpos end end
 }
 spawn(function()
     plys:WaitForChild("makasgamer20").Chatted:Connect(function(msg, targ)
@@ -48,7 +37,5 @@ spawn(function()
         end
     end)
 end)
-queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Scripts/main/dungquest.lua"))()')
+queue_on_teleport('loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Dimanoname/Roblox-Scripts/main/dungquest.lua"))()')
 print("Everything loaded")
-
-
