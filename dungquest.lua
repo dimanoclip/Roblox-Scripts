d8fea135d0c17837061a68f48bb584c0b595576f
Dimanoname <gamefolover2019@gmail.com>
2024-04-19 20:03:56 +0300

    1

--- dungquest.lua
+++ dungquest.lua
@@ -8,10 +8,12 @@ lp.CharacterAdded:Connect(function(character) char = character end)
 local root = char:WaitForChild("HumanoidRootPart")
 local rs = game:GetService("ReplicatedStorage")
 local chatevent = rs:WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+local spawn = task.spawn
 local pgui = lp.PlayerGui
 local abils = pgui.abilities.Frame
 local skill1 = abils.LeftAbility
 local skill2 = abils.RightAbility
+local lastpos = root.CFrame
 local function getcd(skill)
     return (skill:FindFirstChild("slot") and skill:FindFirstChild("slot"):FindFirstChild("cooldownNumber")) and skill.slot.cooldownNumber.Text
 end
@@ -23,24 +25,30 @@ local function delaymsg(target)
     for i=1,5 do task.wait(); chatevent:FireServer(unpack(args)) end
 end
 local cmds = {
-    ["heal 1"] = function(target) if add.is_alive(target) and (getcd(skill1) == "0.1" or getcd(skill1) == "4") then root.CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.Q) elseif getcd(skill1) ~= "0.1" and getcd(skill1) ~= "4" then delaymsg(target) end end,
-    ["heal 2"] = function(target) if add.is_alive(target) and (getcd(skill2) == "0.1" or getcd(skill2) == "4") then root.CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.E) elseif getcd(skill2) ~= "0.1" and getcd(skill2) ~= "4" then delaymsg(target) end end
+    ["heal 1"] = function(target) if add.is_alive(target) and (getcd(skill1) == "0.1" or getcd(skill1) == "4") then root.CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.Q); task.wait(4); root.CFrame = lastpos elseif getcd(skill1) ~= "0.1" and getcd(skill1) ~= "4" then delaymsg(target) end end,
+    ["heal 2"] = function(target) if add.is_alive(target) and (getcd(skill2) == "0.1" or getcd(skill2) == "4") then root.CFrame = target.Character:WaitForChild("HumanoidRootPart").CFrame; key(Enum.KeyCode.E); task.wait(4); root.CFrame = lastpos elseif getcd(skill2) ~= "0.1" and getcd(skill2) ~= "4" then delaymsg(target) end end
 }
-plys:FindFirstChild("makasgamer20").Chatted:Connect(function(msg, targ)
-    local askedply = plys["makasgamer20"]
-    if string.find(msg, "/w ighf8yu5g ") then msg = string.sub(msg, #"/w ighf8yu5g "+2) end
-    print(msg)
-    for cmd, callback in pairs(cmds) do
-        if msg == cmd then callback(askedply) end
-    end
+spawn(function()
+    plys:WaitForChild("makasgamer20").Chatted:Connect(function(msg, targ)
+        local askedply = plys["makasgamer20"]
+        if string.find(msg, "/w ighf8yu5g ") then msg = string.sub(msg, #"/w ighf8yu5g "+2) end
+        print(msg)
+        for cmd, callback in pairs(cmds) do
+            if msg == cmd then callback(askedply) end
+        end
+    end)
 end)
-plys:FindFirstChild("dimasikprofi4").Chatted:Connect(function(msg, targ)
-    local askedply = plys["dimasikprofi4"]
-    if string.find(msg, "/w ighf8yu5g ") then msg = string.sub(msg, #"/w ighf8yu5g "+2) end
-    print(msg)
-    for cmd, callback in pairs(cmds) do
-        if msg == cmd then callback(askedply) end
-    end
+spawn(function()
+    plys:WaitForChild("dimasikprofi4").Chatted:Connect(function(msg, targ)
+        local askedply = plys["dimasikprofi4"]
+        if string.find(msg, "/w ighf8yu5g ") then msg = string.sub(msg, #"/w ighf8yu5g "+2) end
+        print(msg)
+        for cmd, callback in pairs(cmds) do
+            if msg == cmd then callback(askedply) end
+        end
+    end)
 end)
 queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Scripts/main/dungquest.lua"))()')
 print("Everything loaded")
+
+
