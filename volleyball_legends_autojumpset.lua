c781d3ab4f853ac1cf1d94f5fd90891dfcf9335f
dimanoclip <gamefolover2019@gmail.com>
2025-06-24 22:53:22 +0300

    1

--- volleyball_legends_autojumpset.lua
+++ volleyball_legends_autojumpset.lua
@@ -1,14 +1,21 @@
-local ball, closest = nil, {}
+---@diagnostic disable-next-line: unbalanced-assignments
+local ball, closest, settargethl = nil, {nil, 1e99, 1e99}
 h = workspace.ChildAdded:Connect(function(child)
     if child.Name:find("CLIENT_BALL") then
         ball = child
     end
 end)
-local function hlplayer(ply)
+local function hlplayer(ply, fillcolor, outlinecolor, transparency)
+    fillcolor = fillcolor or Color3.fromRGB(0,0,0)
+    outlinecolor = outlinecolor or Color3.fromRGB(255,255,255)
+    transparency = transparency or 0
     local hl = Instance.new("Highlight")
     hl.Parent = ply.Character
+    hl.FillColor = fillcolor
+    hl.OutlineColor = outlinecolor
     hl.DepthMode = Enum.HighlightDepthMode.Occluded
-    hl.FillTransparency = 0.8
+    hl.FillTransparency = transparency
+    return hl
 end
 local lp = game.Players.LocalPlayer
 local char = lp.Character or lp.CharacterAdded:Wait() and lp.Character
@@ -19,7 +26,7 @@ lp.CharacterAdded:Connect(function(character)
     mypp = character.PrimaryPart
 end)
 hlplayer(lp)
-local net = workspace:WaitForChild("Map").BallNoCollide.Net
+local net = workspace:WaitForChild("Map"):WaitForChild("BallNoCollide"):WaitForChild("Net")
 workspace.ChildAdded:Connect(function(child)
     if child.Name == "map" then
         net = child:WaitForChild("BallNoCollide"):WaitForChild("Net")
@@ -29,20 +36,32 @@ task.spawn(function()
     while true do
 		-- pcall(function() char:FindFirstChildOfClass('Highlight').OutlineColor = Color3.fromHSV(tick() % 7.5 / 7.5, 0.6, 0.6) end)
         -- pcall(function() char:FindFirstChildOfClass('Highlight').FillColor = Color3.fromHSV(tick() % 7.5 / 7.5, 0.6, 0.6) end)
-		pcall(function() char:FindFirstChildOfClass('Highlight').OutlineColor = Color3.fromRGB(255,255,255) end)
-        pcall(function() char:FindFirstChildOfClass('Highlight').FillColor = Color3.fromRGB(0,0,0) end)
         pcall(function() workspace:FindFirstChild("BallShadowIndicator").SurfaceGui.Frame.UIStroke.Color = Color3.fromHSV(tick() % 7.5 / 7.5, 0.6, 0.6) end)
         pcall(function() workspace:FindFirstChild("HitIndicator").Color = Color3.fromHSV(tick() % 7.5 / 7.5, 0.6, 0.6) end)
         -- local aim = workspace.Map.BallNoCollide.Positions["2"]["1"]
         -- closest = {aim, (aim.Position-mypp.Position).magnitude}
-        closest = {nil, 1e99, 1e99}
+        if closest[1] then
+            local target = closest[1]
+            local camera_lv =workspace.CurrentCamera.CFrame.LookVector
+            local hit_dist = math.floor((target.Position-(mypp.Position+Vector3.new(camera_lv.X, mypp.CFrame.LookVector.Y, camera_lv.Z)*50)).magnitude)
+            local me_dist = math.floor((target.Position-mypp.Position).magnitude)
+            closest[2] = hit_dist
+            closest[3] = me_dist
+        end
         for i,v in pairs(game.Players:GetChildren()) do task.wait()
             if v.Team ~= lp.Team or v == lp or not v.Character or not net then continue end
             local pp = v.Character.PrimaryPart
-            local net_dist = math.floor((pp.Position-net.Position).magnitude)
+            local camera_lv =workspace.CurrentCamera.CFrame.LookVector
+            local hit_dist = math.floor((pp.Position-(mypp.Position+Vector3.new(camera_lv.X, mypp.CFrame.LookVector.Y, camera_lv.Z)*50)).magnitude)
+            -- local net_dist = math.floor((pp.Position-net.Position).magnitude)
             local me_dist = math.floor((pp.Position-mypp.Position).magnitude)
-            if net_dist < closest[2] then closest = {pp, net_dist, me_dist} end
-            if pp.Position.y > 2 or me_dist < closest[3] then closest = {pp, net_dist, me_dist} end
+            if hit_dist < closest[2] then
+                closest = {pp, hit_dist, me_dist}
+                print(v.Name)
+                if settargethl then settargethl:Destroy() end
+                settargethl = hlplayer(v, Color3.fromRGB(100,40,255), nil, 0.5)
+            end
+            -- if pp.Position.y > 2 or me_dist < closest[3] and me_dist >= 5 then closest = {pp, net_dist, me_dist} end
         end
         task.wait(0.1)
     end
@@ -57,8 +76,8 @@ x = hookmetamethod(game, "__namecall", function(self, ...)
         if closest[1] then
             local target = closest[1]
             local actpos = Vector3.new(target.Position.x, target.Position.y+1.5, target.Position.z)
-            local cf = CFrame.new(actpos + target.CFrame.LookVector*1.4, actpos)
-            if closest[3] <= 5 then tilt = Vector3.yAxis
+            local cf = CFrame.new(actpos + target.CFrame.LookVector*1.5, actpos)
+            if closest[3] <= 3 then tilt = Vector3.yAxis
             else tilt = (cf.Position-ball.PrimaryPart.Position).Unit or tilt end
         end
         return x(self, {
@@ -67,7 +86,7 @@ x = hookmetamethod(game, "__namecall", function(self, ...)
             ["SpecialCharge"] = 1,
             ["TiltDirection"] = tilt,
             ["BallId"] = args[1]["BallId"],
-            ["MoveDirection"] = args[1]["MoveDirection"],
+            ["MoveDirection"] = tilt,
             ["From"] = args[1]["From"],
             ["HitboxSize"] = args[1]["HitboxSize"],
             ["LookVector"] = args[1]["LookVector"]
